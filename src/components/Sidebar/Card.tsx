16a678bfcb632a3d2fad76c2450c2b4e1e2abba8
Полина <polina_kazhina@mail.ru>
2024-02-07 10:47:41 +0300

    feat: add video backgroung to Card

--- src/components/Sidebar/Card.tsx
+++ src/components/Sidebar/Card.tsx
@@ -1,3 +1,5 @@
+import cloudBg from '@/assets/cloudsBg.mp4';
+
 export type Temperature = {
   temperature: number;
   minTemperature: number;
@@ -7,16 +9,19 @@ export type Temperature = {
 function Card({ temperature, minTemperature, maxTemperature }: Temperature) {
   return (
     <div className="card text-white">
-      <div className="flex flex-col pt-[25px] pb-[16px] pl-[20px]">
-        <span className="location pb-[5px]"> My Location </span>
-        <span className="font-normal pb-[59px]"> Minsk </span>
-        <span className="font-normal"> Partly Cloudy </span>
-      </div>
-      <div className="flex flex-col w-[151px] h-[131px] pt-[22px] pb-[22px] mr-[20px]">
-        <div className="celsium"> {temperature.toFixed()}&deg; </div>
-        <div className="diapasone w-[110px]">
-          <span>H:{maxTemperature.toFixed()}&deg;</span>
-          <span>L:{minTemperature.toFixed()}&deg;</span>
+      <video src={cloudBg} autoPlay loop muted className="rounded-[14px]" />
+      <div className="absolute flex justify-between top-[25px] left-[20px] right-[20px]">
+        <div className="flex flex-col h-full">
+          <span className="location pb-[5px]"> My Location </span>
+          <span className="font-normal pb-[59px]"> Minsk </span>
+          <span className="font-normal"> Partly Cloudy </span>
+        </div>
+        <div className="flex flex-col w-[151px] h-[131px] pb-[22px]">
+          <div className="celsium"> {temperature.toFixed()}&deg; </div>
+          <div className="diapasone w-[110px]">
+            <span>H:{maxTemperature.toFixed()}&deg;</span>
+            <span>L:{minTemperature.toFixed()}&deg;</span>
+          </div>
         </div>
       </div>
     </div>
