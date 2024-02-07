8801b055a3f96943deed50c406aacac52db0e030
Полина <polina_kazhina@mail.ru>
2024-02-07 13:04:29 +0300

    fix: add comment

--- src/components/CommonPage/WeatherList/WeatherList.tsx
+++ src/components/CommonPage/WeatherList/WeatherList.tsx
@@ -11,6 +11,7 @@ function WeatherList({ days, minTemp, maxTemp }: Days) {
     <div className="max-w-[535px] list">
       <span className="list-title pb-[10px]"> 10-DAY FORECAST </span>
       {days.map((day, ind) => (
+        // Know about index
         <WeatherItem key={ind} day={day} minTemp={minTemp[ind]} maxTemp={maxTemp[ind]} />
       ))}
       {/* To fix bottom padding */}
